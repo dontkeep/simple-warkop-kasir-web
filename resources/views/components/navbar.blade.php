a01aa2010287d01dc3edcd78ba2c4a0c1e2257a9
dontkeep <alsdiaryy@gmail.com>
2025-05-25 14:01:34 +0700

    update

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar">
     <div class="navbar-content">
         <span class="navbar-title">Warkop Cashier</span>
-        <span class="navbar-subtitle">Simple Laravel + JSON Server Demo</span>
+        <span class="navbar-subtitle">Web Cashier Ryan</span>
     </div>
 </nav>
